807550edd8fd0bf38e578e5359eea3d52d0e2b9d
ProgranadorL <>
2025-04-15 14:49:56 -0500

    'auto-deployed'

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(express.json());
 
 //CORS:
 const corspolicy = {
-  origin: "https://back-end-phonebook.onrender.com/",
+  origin: ["https://back-end-phonebook.onrender.com","http://localhost:5173"],
 };
 
 app.use(cors(corspolicy));
